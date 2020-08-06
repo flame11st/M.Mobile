0bd1078f7298fbde5b7b41dcef97fd52886e8bf7
flame11st <flame11.st@gmail.com>
2020-08-06 21:58:27 +0300

    06.08

--- lib/Widgets/Settings.dart
+++ lib/Widgets/Settings.dart
@@ -1,35 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:mmobile/Variables/Variables.dart';
 
-class Settings extends StatelessWidget{
+import 'Shared/BoxShadowNeomorph.dart';
+import 'Shared/MTextField.dart';
+
+class Settings extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new SettingsState();
+  }
+}
+
+class SettingsState extends State<Settings> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return  Hero(
-            tag: 'settings',
-            child: Material(child: Container(
-                    margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    padding: EdgeInsets.all(20),
-                    color: Theme.of(context).primaryColor,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                            Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                    Icon(Icons.settings, size: 30,),
-                                    SizedBox(
-                                        width: 10,
-                                    ),
-                                    Text('Settings', style: MTextStyles.BodyText,)
-                                ],
-                            ),
-                            RaisedButton(
-                                child: Text('Close'),
-                                onPressed: () => Navigator.of(context).pop(),
-                            )
-                        ],
+    final nameField = MTextField(controller: nameController, text: "Name",);
+    final emailField = MTextField(controller: emailController, text: "Email",);
+
+    return Material(
+        child: Container(
+            margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+            padding: EdgeInsets.all(20),
+            color: Theme.of(context).primaryColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Hero(
+                        tag: 'settings',
+                        child: Icon(
+                          Icons.settings,
+                          size: 30,
+                        )),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Settings',
+                      style: MTextStyles.BodyText,
                     )
-            ))
-    );
+                  ],
+                ),
+                nameField,
+                Divider(
+                  color: Theme.of(context).hintColor,
+                  thickness: 1,
+                  endIndent: 0,
+                ),
+                emailField,
+                RaisedButton(
+                  child: Text('Close'),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            )));
   }
 }
---
+++ lib/Widgets/Shared/MTextField.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:mmobile/Variables/Variables.dart';
+
+class MTextField extends StatelessWidget {
+  final controller;
+  final text;
+
+  MTextField({this.controller, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 15),
+            child: Text(text, style: MTextStyles.Title,),
+          ),
+      Container(
+        margin: EdgeInsets.only(top: 10),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white.withOpacity(0.25),
+                offset: Offset(-4.0, -4.0),
+                blurRadius: 6,
+              ),
+              BoxShadow(
+                color: Colors.black.withOpacity(0.6),
+                offset: Offset(6.0, 6.0),
+                blurRadius: 8,
+              ),
+            ],
+            borderRadius: BorderRadius.circular(32.0),
+            color: Theme.of(context).primaryColor,
+          ),
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32.0))),
+          ))
+    ]);
+  }
+}
