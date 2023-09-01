31b4a66243e07a2256f8666073bd1d1f10428ed7
Serhii Tkachenko <flame11.st@gmail.com>
2023-08-31 20:52:29 -0500

    Update SearchDelegate.dart

--- lib/Widgets/SearchDelegate.dart
+++ lib/Widgets/SearchDelegate.dart
@@ -103,7 +103,7 @@ class MSearchDelegate extends SearchDelegate {
     await Future.delayed(Duration(milliseconds: 2000));
     if (queryToDebounce != query) return;
 
-    if (!userState!.isPremium && userState!.aiRequestsCount % 4 == 0) {
+    if (!userState!.isPremium && userState!.aiRequestsCount % 4 == 0 && ServiceAgent.showLoadingAd) {
       AdManager.showInterstitialAd();
     }
 
@@ -134,7 +134,7 @@ class MSearchDelegate extends SearchDelegate {
       setStateFunction(() => foundMovies = foundMoviesNew);
       globalKey = new GlobalKey();
 
-      userState!.increaseAiRequestsCount();
+      if (ServiceAgent.showLoadingAd) userState!.increaseAiRequestsCount();
 
       notFound = foundMovies.isEmpty;
     }
